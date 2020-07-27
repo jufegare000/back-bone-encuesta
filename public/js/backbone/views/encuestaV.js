f551cb2bb0575a9aec0335925a1beeb29a6bc474
Juan Gallo <jufegare@gmail.com>
2020-07-26 22:33:59 -0500

    datos listos para poblar tabla

--- public/js/backbone/views/encuestaV.js
+++ public/js/backbone/views/encuestaV.js
@@ -1,8 +1,29 @@
 var encuestaV = Backbone.View.extend({
-    el: '#app',
-    initialize: function () {
-        //app.encuestasX.fetch();
+    events: {
+        'click .enviar': 'enviarForm'
+    },
+    enviarForm: function (e) {
+        e.preventDefault();
+        console.log($('#nombre').val());
+        console.log($('#ciudad').val());
+        console.log($('#estres').prop('checked'));
+        console.log($('#nivel').val());
+        console.log($('#ingresos').val());
+        console.log($('#estrato').val());
+        console.log($('#protocolos').prop('checked'));
+        console.log($('#labora').prop('checked'));
+
+        var encuestaNueva = new Encuesta({
+            nombre: $('#nombre').val(),
+            estres: $('#estres').prop('checked'),
+            nivel: $('#nivel').val(),
+            estrato: $('#estrato').val(),
+            protocolos: $('#protocolos').prop('checked'),
+            labora: $('#labora').prop('checked')
+        })
+        encuestasX.add(encuestaNueva);
+        console.log(encuestasX.toJSON());
     }
 });
 
-var appView = new encuestaV();
+var vmm = new encuestaV({ el: $('#botonEnviar') });
